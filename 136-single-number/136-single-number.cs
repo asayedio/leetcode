18aa6786f28a46325d9b9975cf2a4646f7bf6ead
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-07-08 23:28:21 +0200

    Time: 123 ms (78.81%), Space: 39.9 MB (69.71%) - LeetHub

---
+++ 136-single-number/136-single-number.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        int singleNumber = 0;
+        for(int i = 0; i < nums.Length; i++)
+            singleNumber ^= nums[i];
+        return singleNumber;
+    }
+}
